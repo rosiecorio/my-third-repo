90af87ee799f5d26959dff4a25f1464264e74212
Rosie Allison-Corio <rosievespa@gmail.com>
2025-01-09 11:31:17 +0000

    added first two JS workshops

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
-const myName = 'Rosie'
-console.log(myName)
+console.log('Hello World!');
+
+let number = 1;
+console.log(number)
+
+let bananas = 5;
+let strawberries = 3;
+let fruit = bananas + strawberries;
+console.log(fruit);
+
+const myName = `Rosie`;
+const lastName = `Corio`;
+let fullName = `${myName}${lastName}`;
+console.log(fullName)
+console.log(`${myName} is ${myName.length} characters long`)
+
+let isTrue = true;
+console.log(isTrue);
+let isFalse = false;
+console.log(isFalse);
+
+console.log(typeof myName)
+console.log('The type of number is ' + typeof number);
+console.log(`the type of isTrue is ${typeof isTrue}`);
+
+let favouriteProgrammingLanguage = "JavaScript";
+let currentCourseDay = 3;
+
+const sentence = `My favourite programming language is ${favouriteProgrammingLanguage}. We're on day ${currentCourseDay} of the course.`;
+console.log(sentence)
+
+const fruitsentence = `When i make a fruit salad I use ${strawberries} strawberries and ${bananas} bananas`
+console.log(fruitsentence)
